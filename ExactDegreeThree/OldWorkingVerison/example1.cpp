41f1ee61bcb13411443495f8db2800e8ed874cac
Florian <florian@nelles-linz.de>
2016-06-14 15:33:34 +0200

    Added the old version of the exact degree 3 case (with boost and without galois field and not completely integrated in the main program)

---
+++ ExactDegreeThree/OldWorkingVerison/example1.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <armadillo>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/topological_sort.hpp>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <ctime>
+#include "fvs_solver.h"
+#include "lin_parity.h"
+
+using namespace std;
+using namespace arma;
+using namespace fvs;
+
+mat generateIncidenceVector(const Graph& g, const Edge & e, vector<vector<int>>& edgesUsed, int & lastUsedRow,int row_number, const vector<int> & pairNumber, vector<vector<int>>& lastVertexIndex)
+{
+  mat result(row_number,1);
+  result.zeros();
+  int mi=min(source(e,g),target(e,g));
+  int ma=max(source(e,g),target(e,g));
+  edgesUsed[mi][ma]++;
+  result(lastVertexIndex[mi][ma],0)=1;
+  //cout<<mi<<" "<<ma<<" "<<lastVertexIndex[mi][ma]<<endl;
+  if (edgesUsed[mi][ma]==pairNumber[mi]+pairNumber[ma])
+  {
+    result(ma,0)=-1;
+    //cout<<"case 1 "<<ma<<endl;
+
+  }
+  else
+  {
+    result(lastUsedRow,0)=-1;
+    //cout<<lastUsedRow<<" case 2"<<endl;
+    lastVertexIndex[mi][ma]=lastUsedRow;
+    lastUsedRow++;
+  }
+  return result;
+}
+
+pair<mat,vector<Edge>> graphToMatrix(const Graph& g, const set<Node>& u)
+{
+  typedef graph_traits<Graph>::vertices_size_type id;
+
+  typedef graph_traits<Graph>::out_edge_iterator edge_iterator;
+  set<pair<Edge,Edge>> edgePairs;
+  typedef graph_traits<Graph>::vertex_iterator node_iterator;
+
+  map<Node,int> nodeIndex;
+  int index=0;
+  
+  /*pair<node_iterator, node_iterator> nIt = vertices(g);
+  for (node_iterator it = nIt.first; it != nIt.second; ++it)
+  {
+    nodeIndex[*it]=index;
+    index++;
+  }*/
+  vector<int> pairNumber(num_vertices(g),0);
+  for (const auto& n : u) {
+      size_t edgesToOtherUs = 0;
+      vector<Edge> neighbours;
+      pair<edge_iterator, edge_iterator> eIt = out_edges(n, g);
+      for (edge_iterator it = eIt.first; it != eIt.second; ++it) {
+        neighbours.push_back(*it);
+        //edgePairs.insert(make_pair(nodeIndex[i],nodeIndex[target(*it, g)]));
+        //neighbours.push_back(nodeIndex[target(*it, g)]);
+      }
+      pairNumber[n]=neighbours.size()-1;
+      for (int i=0;i<neighbours.size();i++)
+      {
+        for(int j=0;j<i;j++)
+        {
+            edgePairs.insert(make_pair(neighbours[i],neighbours[j]));
+        }
+      }
+  }
+
+  mat matrix;
+  vector<Edge> assignment;
+  int row_number=num_vertices(g)+2*edgePairs.size()-num_edges(g);
+  vector<vector<int>> edgesUsed;
+  vector<vector<int>> lastVertexIndex;
+  for (int i=0;i<num_vertices(g);i++)
+  {
+    vector<int> v(num_vertices(g),0);
+    vector<int> w(num_vertices(g),i);
+    edgesUsed.push_back(v);
+    lastVertexIndex.push_back(w);
+  }
+  int lastUsedRow=num_vertices(g);
+  /*for (int i=0;i<num_vertices(g);i++)
+  {
+    lastVertexIndex[i]=i;
+  }*/
+  for(auto & p: edgePairs)
+  {
+    mat first=generateIncidenceVector(g,p.first,edgesUsed,lastUsedRow,row_number,pairNumber,lastVertexIndex);
+    mat second=generateIncidenceVector(g,p.second,edgesUsed,lastUsedRow,row_number,pairNumber,lastVertexIndex);
+    matrix=join_rows(matrix,first);
+    matrix=join_rows(matrix,second);
+    assignment.push_back(p.first);
+    assignment.push_back(p.second);
+  }
+  return make_pair(matrix,assignment);
+}
+
+vector<int> findRest(const vector <int>& index, int dim)
+{
+    vector<int> result;
+    for(int i=0;i<dim;i++)
+    {
+      if (find(index.begin(), index.end(), i)==index.end())
+      {
+        result.push_back(i);
+      }
+    }
+    return result;
+}
+
+mat extractColumns(const mat & input, const vector<int> & index)
+{
+    mat result;
+    for(int i: index)
+    {
+      result=join_rows(result,input.col(i));
+    }
+    return result;
+}
+
+vector<int> findOnes(const mat & input, int n)
+{
+  vector<int> result;
+  for (int i=0;i<n;i++)
+  {
+      for(int j=0;j<input.n_cols;j++)
+      {
+        if (input.at(i,j)==1)
+        {
+          result.push_back(j);
+          break;
+        }
+      }
+  }
+  return result;
+}
+
+mat rearrangeMatrix(const mat & input, const vector<int> & arrangement)
+{
+  mat result(input.n_rows,input.n_cols);
+  for(int i=0;i<arrangement.size();i++)
+  {
+    result.col(arrangement[i])=input.col(i);
+  }
+  return result;
+}
+
+
+mat transformFullRowRank(mat input)
+{
+  int i=0;
+  int inputRank=arma::rank(input);
+  while(inputRank!=input.n_rows)
+  {
+    mat smallerMat=input;
+    smallerMat.shed_row(i);
+    if (arma::rank(smallerMat)==inputRank)
+    {
+      input=smallerMat;
+    }
+    else
+    {
+      i++;
+    }        
+  }
+  return input;
+}
+
+mat matrixToStadardForm(const mat & input)
+{
+  mat fullRank= transformFullRowRank(input);
+  fullRank.print("transformed row rank");
+  if (fullRank.is_square())
+  {
+    return eye<mat>(fullRank.n_rows,fullRank.n_cols);
+  }
+  mat L, U, P;
+  lu(L,U,P,fullRank.t());
+  L.print("L");
+  U.print("U");
+  P.print("P");
+  mat res=U.t()*L.t()*P;
+  res.print("check");
+  vector<int> invSubMatrix= findOnes(P,L.n_cols);
+  vector<int> restIndex= findRest(invSubMatrix,fullRank.n_cols);
+  mat submatrix=extractColumns(fullRank,invSubMatrix);
+  submatrix.print("sub");
+  mat inverse=submatrix.i();
+  inverse.print("inverse");
+  mat rest= extractColumns(fullRank,restIndex);
+  rest.print("rest");
+  mat newRight=inverse*rest;
+  newRight.print("newRight");
+  vector<int> arrangement=invSubMatrix;
+  arrangement.insert(arrangement.end(), restIndex.begin(), restIndex.end() );
+  mat finalMatrix= join_rows(newRight.t(),eye<mat>(fullRank.n_cols-fullRank.n_rows, fullRank.n_cols-fullRank.n_rows));
+  finalMatrix.print("finalMatrix");
+  return rearrangeMatrix(finalMatrix,arrangement);
+}
+
+int main(int argc, char** argv)
+  {
+  cout << "Armadillo version: " << arma_version::as_string() << endl;
+  std::srand(std::time(0));
+  //mat A;  // directly specify the matrix size (elements are uninitialised)
+  
+  // endr indicates "end of row"
+  //A << 1 << 0 << 0 << endr
+  //  << 0 << 1 << 0 << endr;
+   // << 1 << 2 << 3 << 4 << 5 << endr
+   // << 1 << 2 << 3 << 4 << 7 << endr;
+   // << 1 << 2 << 3 << 4 << 5 << endr;
+  
+  //A.print("A:");
+  
+  //matrixToStadardForm(A).print("Result");
+  
+  Graph g;
+  read_graph(g, "mini_graph.txt");
+  print_graph(g);
+  typedef graph_traits<Graph>::vertex_iterator node_iterator;
+  pair<node_iterator, node_iterator> nIt = vertices(g);
+  set<Node> s;
+  for (node_iterator it = nIt.first; it != nIt.second; ++it)
+  {
+    s.insert(*it);
+  }
+  auto result=graphToMatrix(g,s);
+  result.first.print("IncidenceMatrix");
+  mat instance= matrixToStadardForm(result.first);
+  instance.print("transformed");
+  vector<int> test = simple_parity(instance, 1000);
+ 	for(int i: test)
+  {
+      cout<<"Delete edge from "<<source(result.second[i],g)<< " to "<<target(result.second[i],g) <<endl;
+  }
+   //cout << "Final parity basis are the columns:"<<endl << test << endl;
+  /*for (int i=0;i<vec.n_elem;i++)
+  {
+    int col1=(int) (vec(i)+0.2);
+    int col2=(int) (vec(i)*0.2); 
+    cout<<"Delete edge from "<<source(result.second[col1],g)<< " to "<<target(result.second[col2],g) <<endl;
+  };*/
+  /*for(int i=0;i<test.size();i+=2)
+  {
+      cout<<"Delete note from "<<source(result.second[i],g) <<endl;
+      remove_vertex(source(result.second[i],g),g);
+  }
+  print_graph(g);*/
+  return 0;
+  }
+
---
+++ ExactDegreeThree/OldWorkingVerison/lin_parity.cpp
@@ -0,0 +1,215 @@
+#include "lin_parity.h"
+
+/* NOT NEEDED
+ /* Matrix inversion routine.
+ Uses lu_factorize and lu_substitute in uBLAS to invert a matrix */
+/*
+ * Not needed
+template<class T>
+bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
+{
+	typedef permutation_matrix<std::size_t> pmatrix;
+
+	// create a working copy of the input
+	matrix<T> A(input);
+
+	// create a permutation matrix for the LU-factorization
+	pmatrix pm(A.size1());
+
+	// perform LU-factorization
+	int res = lu_factorize(A, pm);
+	if (res != 0)
+		return false;
+
+	// create identity matrix of "inverse"
+	inverse.assign(identity_matrix<T> (A.size1()));
+
+	// backsubstitute to get the inverse
+	lu_substitute(A, pm, inverse);
+
+	return true;
+}
+/*
+ * Not needed
+int determinant_sign(const permutation_matrix<std::size_t>& pm)
+{
+    int pm_sign=1;
+    std::size_t size = pm.size();
+    for (std::size_t i = 0; i < size; ++i)
+        if (i != pm(i))
+            pm_sign *= -1.0; // swap_rows would swap a pair of rows here, so we change sign
+    return pm_sign;
+}
+ /*
+ * Not needed
+ 
+double determinant( matrix<double>& m ) {
+    permutation_matrix<std::size_t> pm(m.size1());
+    double det = 1.0;
+    if(lu_factorize(m,pm) ) {
+        det = 0.0;
+    } else {
+        for(int i = 0; i < m.size1(); i++) 
+            det *= m(i,i); // multiply by elements on diagonal
+        det = det * determinant_sign( pm );
+    }
+    return det;
+}
+
+*/
+
+
+int get_random_value(int max){
+	int random_variable = std::rand();
+    return random_variable % max + 1 ;
+}
+
+
+
+/* Creates the compact Matrix Y and stores the random values x_i in vector random_values
+ * so random_values has to be a "column of M-half-dim" vec (a pointer of it)
+ * random values are integral between 1 and max_random_value
+ */ 
+mat create_Y(mat M, vec *random_values, int max_random_value){
+	mat Result(M.n_rows, M.n_rows, fill::zeros);
+
+	for(int i = 0 ; i < M.n_cols ; i = i+2){
+
+		if ( (*random_values)(i/2) == 0 ) (*random_values)(i/2) = get_random_value(max_random_value); // if no specific random_value vector is handed, a new random value is assigned 	
+		cout << "Random value ist " << (*random_values)(i/2) << endl;									//if something goes wrong, you can see the random value on the console
+		//random_value = 1;
+		Result = Result + (*random_values)(i/2) * (M.col(i) * M.col(i+1).t() - M.col(i+1)* M.col(i).t() );
+
+	}
+
+	return Result;
+}
+
+
+/*
+ * returns a size-1 dimensional vector (0,1,...,i-1,i+1,..size)
+ */
+Col<uword> every_column_except(int col, int size){//vector muss <uword> sein, um in funktion Y.cols(hilfsvec) eingesetzt werden zu können
+	Col<uword> vector(size-1);
+	int counter = 0;
+	for (int i = 0; i < size; i++ ){
+		if ( i != col){
+		  	vector(counter) = i;
+		  	counter++;
+		}
+	}
+	return vector;
+}
+
+
+mat delete_column(int col, mat matrix){
+	Col<uword> vector = every_column_except(col, matrix.n_cols);
+	return matrix.cols(vector);
+}
+
+
+/*
+ * return the vector of indices, which, if we delete all the columns indices with the return indices, is still of full rank
+ * therefor it goes through all columns, checks if we can delete this column without decreasing the rank
+ */
+Col<uword> find_max_submatrix(mat Y){
+	int counter = 0; //counter for the amount of deleted columns
+	int rank_Y = arma::rank(Y);
+
+	Col<uword> deleted_cols(Y.n_cols - rank_Y);
+	int pos = 0;
+	int columns = Y.n_cols;
+	for(int i = 0; i < columns; i++){
+		
+		mat help = delete_column(i-counter, Y);
+	
+		if (arma::rank(help) == rank_Y){
+			//we can delete column i without loosing rank
+			//store this information
+			deleted_cols(pos) = i;  
+			pos++;
+			//update the Matrix
+			Y = help;
+			counter++;  //store the information, that we deleted a column (so the next col to check is (i-counter))
+		}
+
+		//otherwise if the rank differs, we do nothing and check the next column
+
+	}
+	return deleted_cols;
+
+}
+
+
+/*
+ * input is a vector of indices 
+ * outputs a vector of all indices ascending from 0 to size-1 without those in vec_not
+ */
+Col<uword> all_except(Col<uword> vec_not, int size){
+	Col<uword> vec_result(size-vec_not.n_elem);
+	int pos = 0;			//to count up the vec_not vector (the forbidden indices in vec_not are ascending)
+	int counter = 0;		//to count up the vec_result vector
+
+	for (int i = 0; i < size ; i++){
+		
+		if ((vec_not.n_elem > pos) && (vec_not(pos) == i)){   //the first condition takes care of the problem, that after the last forbidden index, pos is still increased
+			//i is skipped, increase pos to the next forbidden index
+			pos++;
+		}
+		else{
+			//index i is not forbidden, write it to the result-vertex
+			vec_result(counter) = i;
+			counter++;
+		}
+	}
+	return vec_result;
+}
+/*
+ * The main algorithm 4.1 with handeliing of no parity-basis (section 6.5)
+ */
+std::vector<int> simple_parity(mat M, int max_random_value){
+	vec random_values(M.n_cols/2, fill::zeros); 		//stores the randomvalues x_i, created in create_Y (needed later on) 
+	int counter = 0;			   						//for positioning vector_parity 
+
+	cout<<"Algorithm start"<< endl;
+	mat Y = create_Y(M, &random_values, max_random_value);
+
+	M.print("Startmatrix M :");
+	Y.print("Computed Y :");
+
+	int rank_Y = arma::rank(Y);
+	
+	if (rank_Y != Y.n_cols) {
+		cout << endl << endl << "THERE IS NO PARITY BASIS" << endl << "Searching for max rank submatrix of Y" << endl;
+		
+		Col<uword> deleted_cols = find_max_submatrix(Y);   //all columns that can be deleted (and still remain full-rank)
+	
+		deleted_cols = all_except(deleted_cols, Y.n_cols); //all columns that can stay (and still remain full-rank)
+		
+		M = M.rows(deleted_cols);
+
+		M.print("Reduced M (and new instance for parity-basis-algo with less rows):");
+	}
+
+	Y = create_Y(M, &random_values, max_random_value);
+
+	//vec parity_basis(M.n_rows, fill::zeros);		    //in here we will store the indices of the columns that built the parity basis
+	std::vector<int>parity_basis(M.n_rows,0);
+
+	for(int i = 0; i < M.n_cols; i = i+2){
+
+		mat Y_prime = Y - random_values(i/2) *  (M.col(i) * M.col(i+1).t() - M.col(i+1)* M.col(i).t() );
+
+		if (arma::rank(Y_prime) == Y_prime.n_cols){ //if det(Y_prime) != 0
+			Y = Y_prime;
+		}
+		else{
+			//parity_basis(counter) = i;
+			//parity_basis(counter+1) = i+1;
+			parity_basis[counter]=i;
+			parity_basis[counter+1]=i+1;
+			counter = counter + 2;
+		}
+	}
+	return parity_basis;
+}
