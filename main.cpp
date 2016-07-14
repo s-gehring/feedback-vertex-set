1f1086bdd0e4de9ab8f35990d1a31f2a752ed692
Simon Gehring <gehring@cs.uni-bonn.de>
2016-07-14 17:09:55 +0200

    Implemented Articulation vertices. Sorry for the main.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace BinCount;
 * @param [in] g The graph used.
 */
 void output_arti_elems_bridges(const Graph& g) {
-	pair<unordered_set<Node>, unordered_set<Edge> > master_of_arts = g.get_articulation_elements();
+	pair<set<Node>, unordered_set<Edge> > master_of_arts = g.get_articulation_elements();
 	/*
 	**  Output all artiulcation elements.
 	*/
@@ -33,10 +33,10 @@ void output_arti_elems_bridges(const Graph& g) {
 		if (--max_output == 0) { cout << "...[" << master_of_arts.first.size() - MAX_OUTPUT_ARTICULATION << " more]"; break; }
 		if (first_out) {
 			first_out = false;
-			cout << it;
+			cout << g.get_node_name(it);
 		}
 		else {
-			cout << ", " << it;
+			cout << ", " << g.get_node_name(it);
 		}
 	}
 	cout << "}" << endl << endl;
@@ -47,10 +47,10 @@ void output_arti_elems_bridges(const Graph& g) {
 		if (--max_output == 0) { cout << "...[" << master_of_arts.second.size() - MAX_OUTPUT_ARTICULATION << " more]"; break; }
 		if (first_out) {
 			first_out = false;
-			cout << "(" << it.first << "," << it.second << ")";
+			cout << "(" << g.get_node_name(it.first) << "," << g.get_node_name(it.second) << ")";
 		}
 		else {
-			cout << ", (" << it.first << "," << it.second << ")";
+			cout << ", (" << g.get_node_name(it.first) << "," << g.get_node_name(it.second) << ")";
 		}
 	}
 	cout << "}" << endl << endl;
@@ -161,9 +161,8 @@ set<set<Node>> multi_edge_partitions(set<set<Node>>& m, set<Node>& taken,  Graph
 		return_value = m;
 		return_value.insert(taken);
 		return return_value;
-	}
-	// branch on one node
-	else {
+	} else {
+        // branch on one node
 		Node branched_node = adj.begin()->first;
 		// 1) take it
 		Graph h1(g);
@@ -173,9 +172,9 @@ set<set<Node>> multi_edge_partitions(set<set<Node>>& m, set<Node>& taken,  Graph
 		// only remove the edges otherwise other multiedges are destroyed too
 		// this is simply done by removing the node itself
 		h1.remove_node(branched_node);
-		// but we can delete neighbours that are now left alone
+		// but we can delete neighbours that are now left alone or have degree one
 		for (auto &it : adj.begin()->second) {
-			if (h1.get_single_degree(it) == 0) {
+			if (h1.get_single_degree(it) <= 1) {
 				h1.remove_node(it);
 			}
 		}
@@ -211,7 +210,7 @@ int main(int argc, char** argv) {
 	Graph orig(g);
 
 	// preprocessing
-	remove_bridges(g, g.get_articulation_elements().second);
+    remove_bridges(g, g.get_articulation_elements().second);
 
 	//iteratively remove semidisjoint cycles
 	bool progress = true;
@@ -229,65 +228,116 @@ int main(int argc, char** argv) {
 	}
 	debug cout << "Found " << sd_vertices << " vertices of the FVS while removing semidisjoint cycles." << endl;
 	
-	// Split graph on connected components.  
-	std::list<Graph> connected_graphs;
-	get_connected_graphs(g, g.get_connected_components(), connected_graphs);
-	list<set<Node> > partial_solutions;
-	set<Node> complete_solution;
-	for (auto &it : connected_graphs) {
-		debug cout << "Trying to find partial solution for graph " << it.get_name() << "/"<<connected_graphs.size()<<" [n=" << it.get_n() << "|m=" << it.get_m() << "]" << endl;
+    
+    const Graph smaller_orig(g);
+    size_t best_solution = -1;
+    size_t cur_solution; // MAX INT
+    const set<Node> articulation_nodes = smaller_orig.get_articulation_elements().first;
+    Bin_count art_vertices(articulation_nodes.size());
+    
+    set<Node> best_solution_nodes;
+    debug cout << "Found "<<articulation_nodes.size() <<" articulation vertices."<<endl;
+    bool override_ = (articulation_nodes.size() == 0);
+    while(!art_vertices.is_full() || override_) {
+        //Construct g out of orig based on art_vertices.
+        Graph inner_g(smaller_orig);
+        // 0 == is_not_in_fvs.
+        // 1 == is_in_fvs.
+        for(size_t i = 0; i < articulation_nodes.size(); ++i) {
+            if(art_vertices.at(i)) {
+                inner_g.remove_node(*next(articulation_nodes.begin(),i));
+                
+            } else {
+                Node u = *next(articulation_nodes.begin(),i);
+                Neighborhood n = inner_g.get_neighbors(u).first;
+                inner_g.remove_node(u);
+                for(const Node &first_neighbor : n) {
+                    for(const Node &second_neighbor : n) {
+                        if(first_neighbor == second_neighbor) continue;
+                        if(inner_g.has_edge(first_neighbor, second_neighbor)) continue;
+                        if(inner_g.reaches(first_neighbor, second_neighbor)) {
+                            inner_g.add_edge(first_neighbor, second_neighbor);
+                        }
+                    }
+                }
+                
+                
+                
+            }
+        }
+        
+        // Split graph on connected components.  
+        std::list<Graph> connected_graphs;
+        get_connected_graphs(inner_g, inner_g.get_connected_components(), connected_graphs);
+        list<set<Node> > partial_solutions;
+        set<Node> complete_solution;
+        for (auto &it : connected_graphs) {
+            debug cout << "Trying to find partial solution for graph " << it.get_name() << "/"<<connected_graphs.size()<<" [n=" << it.get_n() << "|m=" << it.get_m() << "]" << endl;
 
-		// contract edges and start branching on multiedges within one connected component
-		set<Edge> branching_pairs = contract_edges(it);
-		set<Node> partial_solution;
-		if (branching_pairs.size() > 0) {
-			debug cout << "Start branching on " << branching_pairs.size() << " different multi-edges ";
-			debug cout << "for one connected component." << endl;
-			// create graph by using all multi-edges
-			Graph m;
-			m.add_edges(branching_pairs);
-			// generate all partitions
-			set<Node> taken;
-			set<set<Node>> partitions;
-			partitions = multi_edge_partitions(partitions, taken, m);
-			debug cout << "Reduced size from " << pow(2, branching_pairs.size()) << " to ";
-			debug cout << partitions.size() << " partitions of the graph using the multiedges." << endl;
-			// Bin_count counter(branching_pairs.size());
-			for (set<set<Node>>::iterator partition = partitions.begin(); partition != partitions.end(); ++partition) {
-				Graph h(it);
-				set<Node> current_solution;
-				for (set<Node>::iterator it1 = partition->begin(); it1 != partition->end(); ++it1) {
-					h.remove_node(*it1);
-					current_solution.insert(*it1);
-				}
-				set<Node> help_solution = run_iter_comp(h);
-				current_solution.insert(help_solution.begin(), help_solution.end());
-				// found a new best partial solution using the current multi-edge branching nodes
-				if (current_solution.size() < partial_solution.size() || partial_solution.size() == 0) {
-					partial_solution = current_solution;
-				}
-			}
-		}
-		// if there are no multi-edges, just run iterative compression on the connected component
-		else {
-			debug cout << "There are no multi-edges." << endl;
-			partial_solution = run_iter_comp(it);
-		}
-		partial_solutions.push_back(partial_solution);
-		debug cout << "Found partial solution: ";
-		debug it.print_nodeset(partial_solutions.back());
-		complete_solution.insert(partial_solutions.back().begin(), partial_solutions.back().end());
-	}
-
-	debug cout << "----------------------------------------------" << endl;
-	debug cout << "Computed a total of " << partial_solutions.size() << " partial solutions with ";
-	debug cout << complete_solution.size() << " nodes. Adding " << necessary_nodes.size() << " necessary ";
-	debug cout << "nodes we get a solution size of " << (complete_solution.size() + necessary_nodes.size()) << "." << endl;
-	debug cout << endl;
-	complete_solution.insert(necessary_nodes.begin(), necessary_nodes.end());
-	for (const auto &it : complete_solution) {
+            // contract edges and start branching on multiedges within one connected component
+            set<Edge> branching_pairs = contract_edges(it);
+            set<Node> partial_solution;
+            if (branching_pairs.size() > 0) {
+                debug cout << "Start branching on " << branching_pairs.size() << " different multi-edges ";
+                debug cout << "for one connected component." << endl;
+                // create graph by using all multi-edges
+                Graph m;
+                m.add_edges(branching_pairs);
+                // generate all partitions
+                set<Node> taken;
+                set<set<Node>> partitions;
+                partitions = multi_edge_partitions(partitions, taken, m);
+                debug cout << "Reduced size from " << pow(2, branching_pairs.size()) << " to ";
+                debug cout << partitions.size() << " partitions of the graph using the multiedges." << endl;
+                // Bin_count counter(branching_pairs.size());
+                for (set<set<Node>>::iterator partition = partitions.begin(); partition != partitions.end(); ++partition) {
+                    Graph h(it);
+                    set<Node> current_solution;
+                    for (set<Node>::iterator it1 = partition->begin(); it1 != partition->end(); ++it1) {
+                        h.remove_node(*it1);
+                        current_solution.insert(*it1);
+                    }
+                    set<Node> help_solution = run_iter_comp(h);
+                    current_solution.insert(help_solution.begin(), help_solution.end());
+                    // found a new best partial solution using the current multi-edge branching nodes
+                    if (current_solution.size() < partial_solution.size() || partial_solution.size() == 0) {
+                        partial_solution = current_solution;
+                    }
+                }
+            }
+            // if there are no multi-edges, just run iterative compression on the connected component
+            else {
+                debug cout << "There are no multi-edges." << endl;
+                partial_solution = run_iter_comp(it);
+            }
+            partial_solutions.push_back(partial_solution);
+            debug cout << "Found partial solution: ";
+            debug it.print_nodeset(partial_solutions.back());
+            complete_solution.insert(partial_solutions.back().begin(), partial_solutions.back().end());
+            
+        }
+        cur_solution = complete_solution.size() + art_vertices.get_true_values();
+        if(cur_solution < best_solution) {
+            best_solution_nodes.clear();
+            best_solution_nodes.insert(complete_solution.begin(), complete_solution.end());
+            for(size_t i = 0; i < articulation_nodes.size(); ++i) {
+                if(art_vertices.at(i)) {
+                    best_solution_nodes.insert(*next(articulation_nodes.begin(),i));
+                }
+            }
+            debug cout << "Found better solution than "<<best_solution<<"." <<endl;
+            debug cout << "We add fix "<< art_vertices.get_true_values()<< " nodes because of ";
+            debug cout << "articulation vertices and add "<< complete_solution.size()<<" nodes."<<endl;;
+            best_solution = cur_solution;
+        }
+        art_vertices.increase();
+        if(override_)break;
+    }
+	
+	best_solution_nodes.insert(necessary_nodes.begin(), necessary_nodes.end());
+	for (const auto &it : best_solution_nodes) {
 		cout << node_names.second[it] << endl;
 	}
-	debug cout << "Sanity check: " << (is_fvs(orig, complete_solution) ? "PASS" : "FAILED") << endl;
+	debug cout << "Sanity check: " << (is_fvs(orig, best_solution_nodes) ? "PASS" : "FAILED") << endl;
 	debug cout << "--------------- END OF PROGRAM ---------------" << endl;
 }
